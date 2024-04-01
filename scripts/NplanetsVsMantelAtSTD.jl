3c3816a430c8f932a2c28e97a4d86d0125c7fac7
hbsmith <hbs@asu.edu>
2024-04-01 18:00:50 +0900

    add julia scripts

---
+++ scripts/NplanetsVsMantelAtSTD.jl
@@ -0,0 +1,239 @@
+## NplanetsVsMantelAtSTD.jl
+## Recreates Fig. A3 in Smith & Sinapayen, 2024 
+## https://arxiv.org/abs/2403.14195
+## Written by Harrison B. Smith, 2024
+## Instructions:
+## > julia 
+## > include("NplanetsVsMantelAtSTD.jl")
+## > main()
+
+using Pkg
+println(@__DIR__)
+Pkg.activate(@__DIR__)
+
+using Revise
+using Plots
+using Random
+# Pkg.develop(path=".") #../TerraformingAgents" ## Don't need this if I start the Julia REPL from the package itself
+using TerraformingAgents
+using Distances
+using LinearAlgebra: diag, issymmetric, tril!
+using Statistics
+
+"""
+    PermuteDistanceMatrix(D; rng::AbstractRNG = Random.default_rng())
+
+Randomly permute the rows and columns of a distance matrix `D`.
+"""
+function PermuteDistanceMatrix(D; rng::AbstractRNG = Random.default_rng())
+    order = shuffle(rng, collect(1:size(D)[1]))
+    return D[order,:][:,order]
+end
+
+"""
+    triangleFlatten(D)
+
+Flatten the lower triangle of a square matrix `D` into a 1D array.
+"""
+triangleFlatten(D) = D[tril!(trues(size(D)), -1)]
+
+
+"""
+    MantelTestPermutations(
+        x, 
+        y; 
+        rng::AbstractRNG=Random.default_rng(), 
+        dist_metric=Euclidean(), 
+        method=:pearson, 
+        permutations=999, 
+        alternative=:twosided)
+
+Perform a Mantel test between two distance matrices `x` and `y` with `permutations` 
+    permutations. Based off of: 
+    https://github.com/biocore/scikit-bio/blob/0.1.3/skbio/math/stats/distance/_mantel.py#L23
+
+Returns the original statistic (the mantel coefficient), the p-value, and the 
+    permuted statistics (the mantel coefficients of the permutations).
+
+
+"""
+function MantelTestPermutations(
+    x, 
+    y; 
+    rng::AbstractRNG=Random.default_rng(), 
+    dist_metric=Euclidean(), 
+    method=:pearson, 
+    permutations=999, 
+    alternative=:twosided
+    )
+
+    ## Check validity of arguments
+    method == :pearson ? corr_func = cor : throw(ArgumentError("Not yet implemented"))
+    permutations < 0 && throw(ArgumentError("Number of permutations must be greater than or equal to zero."))
+    alternative ∉ [:twosided, :greater, :less] && throw(ArgumentError("Invalid alternative hypothesis $alternative."))
+
+    ## Check x and y to verify they're distance matrices
+    size(x) != size(y) && throw(ArgumentError("Distance matrices must have the same shape."))
+    size(x)[1] < 3 && throw(ArgumentError("Distance matrices must be at least 3x3 in size."))
+    sum(abs.(diag(x))) + sum(abs.(diag(y))) != 0 && throw(ArgumentError("Distance matrices must be hollow."))
+    ~issymmetric(x) | ~issymmetric(y) && throw(ArgumentError("Distance matrices must be symmetric."))
+
+    ## This part just needs to get a flattened version of the diagonal of a hollow, square, symmetric matrix
+    x_flat = triangleFlatten(x)
+    y_flat = triangleFlatten(y)
+
+    orig_stat = corr_func(x_flat, y_flat)
+
+    ## Permutation tests
+    if (permutations == 0) | isnan(orig_stat)
+        p_value = NaN
+        permuted_stats = NaN
+    else
+        perm_gen = (cor(triangleFlatten(PermuteDistanceMatrix(x, rng=rng)), y_flat) for _ in 1:permutations)
+        permuted_stats = collect(Iterators.flatten(perm_gen))
+
+        if alternative == :twosided
+            count_better = sum(abs.(permuted_stats) .>= abs(orig_stat))
+        elseif alternative == :greater
+            count_better = sum(permuted_stats .>= orig_stat)
+        else
+            count_better = sum(permuted_stats .<= orig_stat)
+        end
+
+        p_value = (count_better + 1) / (permutations + 1)
+
+    end
+
+    return orig_stat, p_value, permuted_stats
+
+end
+
+"""
+    MantelTestCompositionsPositions(
+        comp, 
+        pos; 
+        dist_metric=Euclidean(), 
+        rng::AbstractRNG=Random.default_rng(), 
+        method=:pearson, 
+        permutations=999, 
+        alternative=:twosided)
+
+Perform a Mantel test specifically between the composition matrix and position matrix as 
+    they are formatting in the TerraformingAgents initialization.
+
+Returns MantelTestPermutations ouputs.
+"""
+function MantelTestCompositionsPositions(
+    comp, 
+    pos; 
+    dist_metric=Euclidean(), 
+    rng::AbstractRNG=Random.default_rng(), 
+    method=:pearson, 
+    permutations=999, 
+    alternative=:twosided
+    )
+
+    x = pairwise(dist_metric, comp, dims=2)
+    y = pairwise(dist_metric, hcat(collect.(pos)...), dims=2)
+
+    MantelTestPermutations(x, y; rng=rng, dist_metric=dist_metric, method=method, permutations=permutations, alternative=alternative)
+
+end
+
+"""
+    MantelTestTerraformingAgents(
+        rng::AbstractRNG=Random.default_rng(), 
+        extent=(100, 100, 100), 
+        nplanets=1000, 
+        maxcomp=1, 
+        compsize=10, 
+        permutations=999, 
+        alternative=:twosided)
+
+Initialize from the TerraformingAgents simulation the composition and position matrices, 
+    and perform a Mantel test between them.
+
+Returns MantelTestPermutations ouputs.
+"""
+function MantelTestTerraformingAgents(;
+    rng = MersenneTwister(3141),
+    extent = (100, 100, 100),
+    nplanets = 1000,
+    maxcomp = 1,
+    compsize = 10,
+    permutations = 999,
+    alternative=:twosided
+    )
+    
+    pos = TerraformingAgents.random_positions(rng, extent, nplanets)
+    comp = TerraformingAgents.random_compositions(rng, maxcomp, compsize, nplanets)
+    orig_stat, p_value, permuted_stats = MantelTestCompositionsPositions(comp, pos; dist_metric=Euclidean(), rng=rng, method=:pearson, permutations=permutations, alternative=alternative)
+    
+end
+
+"""
+    NplanetsVsMantelAtSTD(nplanet_range, std_vals; kwargs...)
+
+Calculate what Mantel coefficient corresponds to the sigma anomalies provided in 
+    `std_vals`, for a range of nplanets present and observed in the simulation 
+    `nplanet_range`.
+
+Returns a 2D matrix of Mantel coefficients for each sigma value in `std_vals` and 
+    each nplanet in `nplanet_range`.
+    
+"""
+function NplanetsVsMantelAtSTD(nplanet_range, std_vals; kwargs...)
+#permutations = 500 should be good
+    mantels = []
+    for nplanets in nplanet_range
+        @show nplanets
+        o, p, ps = MantelTestTerraformingAgents(nplanets=nplanets; kwargs...)
+        std1 = Statistics.stdm(ps, mean(ps))
+        std2345 = std1 .* std_vals
+        push!(mantels, std2345)
+    end
+    
+    hcat(mantels...)
+
+end
+
+"""
+    PlotNplanetsVsMantelAtSTD(mantels, nplanet_range, std_vals; kwargs...)
+
+Returns a plot of the Mantel coefficients for each sigma value in `std_vals` and each nplanet in
+    `nplanet_range`, calcuated from `NplanetsVsMantelAtSTD`.
+"""
+function PlotNplanetsVsMantelAtSTD(mantels, nplanet_range, std_vals; kwargs...)
+    labels = collect(string(i)*"σ" for i in std_vals)
+    
+    ## Plot
+    P = plot(
+        xlabel = "Number of planets",
+        ylabel = "log(Mantel coefficient)",
+    )
+    for i in 1:length(std_vals)
+        plot!(
+            P, 
+            nplanet_range, 
+            mantels[i,:], 
+            label=labels[i], 
+            palette=:darkrainbow, 
+            marker=(:circle,5), 
+            yaxis=:log10,
+            xticks=[10,100,250,500,1000,2000],
+            # xminorticks=nplanet_range,
+            minorgrid=true)
+    end
+    P
+
+end
+
+## Creates plot seen in the appendix of the paper
+function main()
+    std_vals = [2.5, 5]
+    nplanet_range = [5, 10, 25, 75, 100, 250, 500, 1000, 2000]
+    mantels = NplanetsVsMantelAtSTD(nplanet_range, std_vals; permutations=1000)
+    P = PlotNplanetsVsMantelAtSTD(mantels, nplanet_range, std_vals) 
+    gui(P)
+    # savefig(PlotNplanetsVsMantelAtSTD(mantels, nplanet_range, std_vals), "../../nplanets_vs_mantel_at_sigma_test.pdf")
+end
---
+++ scripts/run_simulation_parallel.jl
@@ -0,0 +1,243 @@
+## run_simulation_parallel.jl
+## Collect data from a run with 1000 planets for 1000 timesteps
+## Uses parallel processing
+## Generates data found here: [FIGSHARE]
+## Written by Harrison B. Smith, 2024
+## Instructions (will output files to TerraformingAgents_Projects/output/):
+## > julia
+## > include("run_simulation_parallel.jl")
+
+## Set number of cores to use
+using Distributed
+desired_n_workers = 7
+
+## Below code makes sure you don't accidentally spawn too many procs when calling code 
+##  using include()
+worker_diff = desired_n_workers - nworkers()
+if worker_diff > 0
+    addprocs(worker_diff)
+elseif worker_diff < 0
+    rmprocs(filter!(x -> x > desired_n_workers + 1, workers()))
+end
+@show workers()
+
+## Must activate independently of imports
+@everywhere begin
+    using Pkg
+    println(@__DIR__)
+    Pkg.activate(@__DIR__)
+end
+@everywhere begin
+    using Revise
+    using Agents, Plots, Random
+    using TerraformingAgents
+    using CSVFiles
+    using DataFrames
+    using Dates
+    using DrWatson: @dict
+    using JSON
+    using YAML
+    using OrderedCollections
+
+    n = 1000 # number of steps to run simulation for
+    parameters = Dict(
+        :nplanets => 1000,
+        :extent => (100,100,100),
+        :nool => [1,10],
+        :dt => 10,
+        :maxcomp => 1,
+        :compsize => 10,
+        :spawn_rate => 0.01,
+        :agent_step! => galaxy_agent_step_spawn_at_rate!,
+        :compmix_func => horizontal_gene_transfer,
+        :mutation_rate => [0,0.1],
+        :n_idxs_to_keep_from_destination => [0,1,2,5],
+        :compatibility_func => planets_in_range,
+        :r => [
+            9,
+            10,
+            11,
+            12,
+            20,
+            50,
+            174],
+        :destination_func => most_similar_planet,
+        :rng => MersenneTwister(3141)
+        )
+
+end
+
+"""
+    write_parameters_to_json(parameters, n)
+
+Write the parameters to a JSON and YAML file in the output directory
+
+# Arguments
+- `parameters::Dict`: A dictionary of parameters to write to file
+- `n::Int`: The number of steps to run the simulation for
+
+# Returns
+- `pathname::String`: The path to the directory where the parameters were written
+"""
+function write_parameters_to_json(parameters, n)
+    pkg_path = joinpath(splitpath(pathof(TerraformingAgents))[1:end-2])
+    curr_time = Dates.format(now(),"YYYY-mm-dd-HHMMSS")
+    pathname = joinpath(pkg_path,"output",curr_time)
+    mkpath(pathname)
+    parameters_copy = deepcopy(OrderedDict(parameters))
+    parameters_copy[:compmix_func] = string(parameters_copy[:compmix_func])
+    parameters_copy[:compatibility_func] = string(parameters_copy[:compatibility_func])
+    parameters_copy[:destination_func] = string(parameters_copy[:destination_func])
+    parameters_copy[:agent_step!] = string(parameters_copy[:agent_step!])
+    parameters_copy[:n] = n
+    parameters_copy = sort(parameters_copy)
+    # Write JSON of parameters to file
+    open(joinpath(pathname,"parameters.json"),"w") do f
+        JSON.print(f, parameters_copy, 4) 
+    end
+    YAML.write_file(joinpath(pathname,"parameters.yml"), parameters_copy)
+    return pathname 
+end
+pathname = write_parameters_to_json(parameters, n)
+
+@everywhere begin
+    """
+        hgt_paramscan_setup(;kwargs...)
+
+    Setup function for the parameter scan. This function is called by the `paramscan` 
+        function in Agents.jl
+
+    NOTE: It doesn't matter what these default values are because they will all be 
+          overwritten by the parameters argument when paramscan is called
+
+    # Returns
+    - `model::GalaxyModel`: The model to run the simulation on
+    """
+    function hgt_paramscan_setup(;
+        nplanets = 100,
+        extent = (100,100,100),
+        nool = 1,
+        dt = 10,
+        maxcomp = 1,
+        compsize = 4,
+        spawn_rate = 0.01,
+        agent_step! = galaxy_agent_step_spawn_at_rate!,
+        compmix_func = horizontal_gene_transfer,
+        mutation_rate = 0,
+        n_idxs_to_keep_from_destination = 2,
+        compatibility_func = compositionally_similar_planets,
+        allowed_diff = nothing,
+        r = nothing,
+        destination_func = nearest_planet,
+        rng = MersenneTwister(3141)
+        )
+        ## 
+
+        compmix_kwargs = Dict(
+            :mutation_rate=>mutation_rate, 
+            :n_idxs_to_keep_from_destination=>n_idxs_to_keep_from_destination)
+
+        if compatibility_func == compositionally_similar_planets
+            compatibility_kwargs = Dict(:allowed_diff=>allowed_diff)
+        elseif compatibility_func == planets_in_range
+            compatibility_kwargs = Dict(:r=>r)
+        end
+
+        galaxyparams = GalaxyParameters(
+            rng,
+            nplanets,
+            extent = extent,
+            nool = nool,
+            dt = dt,
+            maxcomp = maxcomp,
+            compsize = compsize,
+            spawn_rate = spawn_rate,
+            compmix_func = compmix_func,
+            compmix_kwargs = compmix_kwargs,
+            compatibility_func = compatibility_func,
+            compatibility_kwargs = compatibility_kwargs,
+            destination_func = nearest_planet)
+        
+        model = galaxy_model_setup(galaxyparams)
+
+        model
+
+    end
+
+    ## Functions specific to agent data collection. 
+    ##  The main purpose is to turn references to objects into references to object ids
+    destination_id(A)::Int = A.destination.id
+    ancestor_ids(A)::Vector{Int} = [i.id for i in A.ancestors]
+    adata =  [:pos,
+            :composition, # property of Planet and Life
+            :alive,
+            :claimed,
+            :destination_distance, # Life
+            destination_id, # Life
+            ancestor_ids] # Life
+    adata = filter(x -> Symbol(x) ∉ keys(parameters), adata)
+
+    ## Functions specific to model data collection.
+    mantel_corr_coeff(model) = TerraformingAgents.PlanetMantelTest(model; permutations=0)[1] ## Permutations set to 0 because mdata won't be able to show this p-value anyways
+    mantel_p_value(model) = TerraformingAgents.PlanetMantelTest(model; permutations=99)[2] ## 99 permutations is relatively low, but will significantly speed up the simulation
+    mutation_rate(model) = model.properties[:compmix_kwargs][:mutation_rate]
+    n_idxs_to_keep_from_destination(model) = model.properties[:compmix_kwargs][:n_idxs_to_keep_from_destination]
+    allowed_diff(model) = model.properties[:compmix_kwargs][:allowed_diff]
+    r(model) = model.properties[:compmix_kwargs][:r]
+    mdata = [:dt,
+            :lifespeed, 
+            :interaction_radius, 
+            :nplanets, 
+            :maxcomp, 
+            :compsize,
+            :spawn_rate,
+            :compmix_func,
+            :n_living_planets,
+            :terraformed_on_step,
+            :n_terraformed_on_step,
+            :compatibility_func,
+            :destination_func,
+            mutation_rate,
+            n_idxs_to_keep_from_destination,
+            mantel_corr_coeff,
+            mantel_p_value]
+    if parameters[:compatibility_func] == compositionally_similar_planets
+        push!(mdata, allowed_diff)
+    elseif parameters[:compatibility_func] == planets_in_range
+        push!(mdata, r)
+    end
+    mdata = filter(x -> Symbol(x) ∉ keys(parameters), mdata)
+
+    ## the paramscan function requires the step functions to be named exactly agent_step! and model_step!
+    agent_step! = parameters[:agent_step!]
+    model_step! = galaxy_model_step!
+
+    ## only collect data on steps where planets are terraformed
+    terraformed_on_step(model,s) = model.terraformed_on_step
+
+end
+
+df_agent, df_model = paramscan(
+                        parameters, 
+                        hgt_paramscan_setup; 
+                        agent_step!, 
+                        model_step!, 
+                        n,
+                        adata=adata, 
+                        mdata=mdata,
+                        when=terraformed_on_step,
+                        when_model=terraformed_on_step,
+                        showprogress=true,
+                        obtainer=deepcopy,
+                        include_constants = false,
+                        parallel=true)
+
+## Agents.jl combines all all agent data in a single dataframe, but we want to separate 
+##  "planets" from "life", as they have many non-overlapping and empty properties that 
+##  makes data processing cumbersome
+df_planets, df_lifes = split_df_agent(df_agent, length(parameters[:extent]), parameters[:compsize])
+
+save(File(format"CSV", joinpath(pathname, "df_planets.csv.gz")), clean_df(df_planets), delim=';')
+save(File(format"CSV", joinpath(pathname, "df_lifes.csv.gz")), clean_df(df_lifes), delim=';')
+save(File(format"CSV", joinpath(pathname, "df_model.csv.gz")), clean_df(df_model), delim=';')
+
